e84a7e7870d87b7a3c70371889c4db4a5a14dd9a
steven-haddad <steven-haddad.2@hotmail.fr>
2017-12-12 23:29:51 +0100

    -Spinner populate with api rest response -Add service to check when user lost his connection -Custom Menu

--- app/src/main/java/com/example/haddad/managemyrounds/controller/fragment/displayFacesFragment.java
+++ app/src/main/java/com/example/haddad/managemyrounds/controller/fragment/displayFacesFragment.java
@@ -1,18 +1,30 @@
 package com.example.haddad.managemyrounds.controller.fragment;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.haddad.managemyrounds.R;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class displayFacesFragment extends Fragment {
 
     public static final String TITLE = "Faces";
+    public String selectedPeriod;
+    public Set<String>  idFurnitures;
+
+
     public displayFacesFragment() {
 // Required empty public constructor
     }
@@ -22,13 +34,47 @@ public class displayFacesFragment extends Fragment {
     }
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_display_faces, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup containerFaces, Bundle savedInstanceState) {
+        View fragmentHandle = inflater.inflate(R.layout.fragment_display_faces, containerFaces, false);
+        Button button= (Button) fragmentHandle.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSelectedFurnitures();
+            }
+        });
+
+
+        return inflater.inflate(R.layout.fragment_display_faces, containerFaces, false);
     }
 
     public static displayFacesFragment newInstance() {
         return new displayFacesFragment();
 
     }
+
+
+
+
+    public void getSelectedPeriod(){
+
+        SharedPreferences myPrefs = this.getActivity().getSharedPreferences("selectedPeriod", MODE_PRIVATE);
+        selectedPeriod = myPrefs.getString("selectedPeriod", null);
+        String selectedPeriod1=selectedPeriod;
+    }
+
+
+    public void getSelectedFurnitures(){
+
+        Bundle bundle = getArguments();
+        ArrayList<String> a1= bundle.getStringArrayList("selectedFurnitures");
+
+
+
+    }
+
+
+
+
 
 }
--- app/src/main/java/com/example/haddad/managemyrounds/controller/fragment/displayFurnituresFragment.java
+++ app/src/main/java/com/example/haddad/managemyrounds/controller/fragment/displayFurnituresFragment.java
@@ -1,5 +1,6 @@
 package com.example.haddad.managemyrounds.controller.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,6 +8,8 @@ import android.content.res.TypedArray;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -36,8 +39,10 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -67,6 +72,9 @@ public class displayFurnituresFragment extends Fragment {
 
     CharSequence mLabel;
     View v;
+    ArrayList<String> listIdFurnitures;
+    String stringIdFurniture;
+
 
 
     @Override
@@ -74,19 +82,76 @@ public class displayFurnituresFragment extends Fragment {
 
         View fragmentHandle = inflater.inflate(R.layout.fragment_display_furnitures, container, false);
         spinnerPostingPeriods= (Spinner) fragmentHandle.findViewById(R.id.spinnerPostingPeriod2);
+        mListView = (ListView) fragmentHandle.findViewById(R.id.listViewRound2);
 
+        getPostingPeriodsInfomations();
         spinnerPostingPeriods.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("youpi","wouhouuuuuuuuuuu");
+
+                String selected = adapterView.getItemAtPosition(i).toString();
+                String[] periods=selected.split(" ");
+                selectedPeriod=periods[0];
+                getFurnitureInfomations(selectedPeriod);
+                //Share the selected posting period with other activities
+                shareSelectedPeriod(selectedPeriod);
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 //No event
             }
         });
-           ;
 
+        getPostingPeriodsInfomations();
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+
+                TextView idFurniture = (TextView) view.findViewById(R.id.idFurniture);
+                stringIdFurniture = idFurniture.getText().toString();
+
+
+              listIdFurnitures = new ArrayList<String>();
+
+
+
+
+
+                shareSelectedFurnitures(listIdFurnitures);
+
+
+
+
+
+
+                /* Intent intentDisplayFaces = new Intent(getActivity(), displayFacesFragment.class);
+                intentDisplayFaces.putExtra("roundFurnCode", stringIdFurniture);
+                startActivity(intentDisplayFaces );*/
+
+
+               // Fragment newFragment = new displayFacesFragment();
+                //FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+                //transaction.replace(R.id.mainframe, newFragment);
+               // transaction.addToBackStack(null);
+
+// Commit the transaction
+                //transaction.commit();
+
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //No event
+            }
+        });
+           ;
+        shareSelectedFurnitures(listIdFurnitures);
         return fragmentHandle;
     }
 
@@ -105,6 +170,162 @@ public class displayFurnituresFragment extends Fragment {
 
     }
 
+
+    public void getPostingPeriodsInfomations() {
+
+        String cancel_req_tag = "Posting Periods";
+        String URL_API_REST_DISPLAY_POSTING_PERIODS = "http://managemyround.livehost.fr/JSON/getAllValidPostingPeriods.php";
+        Log.e("Url", URL_API_REST_DISPLAY_POSTING_PERIODS);
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                URL_API_REST_DISPLAY_POSTING_PERIODS, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+
+                try {
+                    JSONObject o = new JSONObject(response);
+
+                    jsonArrayWeeks = o.getJSONArray("week");
+                    jsonArrayFromDates = o.getJSONArray("from_date");
+                    jsonArrayToDates = o.getJSONArray("to_date");
+
+                    arrSizePostingPeriod = jsonArrayWeeks.length();
+                    postingPeriods = new ArrayList<String>();
+
+                    for (int i = 0; i <= arrSizePostingPeriod; i++) {
+                        try {
+                            postingPeriods.add(jsonArrayWeeks.getString(i)+"  :"+jsonArrayFromDates.getString(i)+" - "+jsonArrayToDates.getString(i));
+                            Log.i("t", String.valueOf(postingPeriods));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        populateSpinnerPostingPeriods();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getActivity().getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+        };
+        // Adding request to request queue
+
+        AppSingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
+    }
+
+    private void populateSpinnerPostingPeriods()  {
+
+
+        spinnerPostingPeriods.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, postingPeriods));
+        java.util.ArrayList<String> strings = new java.util.ArrayList<>();
+    }
+
+      /*
+    Populate furnitures list view
+
+    getFurnituresInformations : Https request to retrieve all furnitures (criterias: Posting period selected,id_agent)
+    generateFurnitures: populate Furnitures list with json
+    displayFurnituresList:  populate the list view  with furnitures list
+
+     */
+
+    private List<Furniture> generateFurnitures() throws JSONException {
+        // Tag used to cancel the request
+        final List<Furniture> furnitures = new ArrayList<Furniture>();
+
+        for (int i = 0; i < arrSizeFurniture; ++i) {
+            furnitures.add(new Furniture(jsonArrayPostingDay.getString(i), jsonArrayNumberOfFaces.getString(i), jsonArrayRegion.getString(i), jsonArrayFurnCode.getString(i)));
+        }
+        return furnitures;
+    }
+
+    private void displayFurnituresList() {
+        List<Furniture> furnitures = null;
+        try {
+            furnitures = generateFurnitures();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        FurnitureListAdapter adapter = new FurnitureListAdapter(getActivity(), furnitures);
+        mListView.setAdapter(adapter);
+
+    }
+
+    private void getFurnitureInfomations(final String periodSelected) {
+
+        String cancel_req_tag = "FACE";
+        String URL_API_REST_DISPLAY_FACE = "http://managemyround.livehost.fr/JSON/retrieveAllFurnitureByPeriod.php?selectedPeriod=" + periodSelected;
+        Log.e("Url", URL_API_REST_DISPLAY_FACE);
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                URL_API_REST_DISPLAY_FACE, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+
+                try {
+                    JSONObject o = new JSONObject(response);
+
+                    jsonArrayFurnCode = o.getJSONArray("furnCode");
+                    jsonArrayNumberOfFaces = o.getJSONArray("numberOfFaces");
+                    jsonArrayRegion = o.getJSONArray("region");
+                    jsonArrayPostingDay = o.getJSONArray("postingDay");
+                    arrSizeFurniture = jsonArrayFurnCode.length();
+
+                    displayFurnituresList();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getActivity().getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        })
+
+        {
+            protected Map<String, String> getParams() {
+                // Posting params to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("selectedPeriod", periodSelected);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
+    }
+
+
+    public void shareSelectedPeriod(String selectedPeriod){
+        SharedPreferences myPrefs = this.getActivity().getSharedPreferences("selectedPeriod", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = myPrefs.edit();
+        prefsEditor.putString("selectedPeriod", selectedPeriod);
+        prefsEditor.commit();
+
+    }
+
+
+    public void shareSelectedFurnitures(ArrayList<String> selectedFurnitures){
+        Bundle args = new Bundle();
+        args.putStringArrayList("selectedFurnitures",selectedFurnitures);
+        displayFacesFragment newFragment = new displayFacesFragment ();
+        newFragment.setArguments(args);
+      selectedFurnitures.add(String.valueOf(this.jsonArrayFurnCode));
+
+    }
 
 }
 
